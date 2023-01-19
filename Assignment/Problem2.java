81c79235ac55af091f5bbde263c312e69de61e29
rajeshpradhan007 <rajeshpradhan04@gmail.com>
2023-01-19 23:18:16 +0530

    Update Problem2.java

--- Assignment/Problem2.java
+++ Assignment/Problem2.java
@@ -1,1 +1,13 @@
-dd
+public class HelloWorld {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=0;j<4;j++){
+                
+                    System.out.print(i);
+                
+                
+            }
+            System.out.print("\n");
+        }
+    }
+}
